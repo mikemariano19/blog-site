1395af9a37885ba1b881e5a992441b053611525a
Mike Mariano <52498452+mikemariano19@users.noreply.github.com>
2025-01-22 23:24:48 +0800

    Setting up backend

---
+++ backend/routes/routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+// GET method route
+app.get('/', (req, res) => {
+    res.send('GET request to the homepage')
+  })
+  
+  // POST method route
+  app.post('/', (req, res) => {
+    res.send('POST request to the homepage')
+  })
